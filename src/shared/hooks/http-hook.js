982c3c9925837776123520261a5e9edc1e143614
Jeremy Crichton <jeremy_crichton@hotmail.com>
2019-12-22 09:41:17 +0800

    update http hook and refactor auth

--- src/shared/hooks/http-hook.js
+++ src/shared/hooks/http-hook.js
@@ -1,31 +1,48 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useRef, useEffect } from 'react';
 
 export const useHttpClient = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
+  // A piece of data that wont' be reinitialized when this fn runs again (whenever component that uses this hook rerenders)
+  // - stores data across re-render cycles
+  const activeHttpRequests = useRef([]);
+
   // Wrap this fn w/ useCallback so fn never gets recreated when the component that uses this hook re-renders
+  // - elmininates possibility of inefficient re-render cycles or an infinite loop
   const sendRequest = useCallback(
     async (url, method = 'GET', body = null, headers = {}) => {
       setIsLoading(true);
+
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequests.current.push(httpAbortCtrl);
+
       try {
-        const response = fetch(url, {
+        const response = await fetch(url, {
           method,
           body,
-          headers
+          headers,
+          signal: httpAbortCtrl.signal
         });
 
         const responseData = await response.json();
+
+        // Keep every controller except controller used in this request
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          reqCtrl => reqCtrl !== httpAbortCtrl
+        );
 
         if (!response.ok) {
           throw new Error(responseData.message);
         }
 
+        setIsLoading(false);
         return responseData;
       } catch (err) {
         setError(err.message);
+        setIsLoading(false);
+        throw err;
       }
-      setIsLoading(false);
     },
     []
   );
@@ -33,6 +50,15 @@ export const useHttpClient = () => {
   const clearError = () => {
     setError(null);
   };
+
+  // all this abort ctrl logic is so wwe don't continue w/ a request that is on its way out if we switch away from component that triggered the request
+  useEffect(() => {
+    // fn w/in useEffect fn executes as a cleanup fn before useEffect runs again
+    // OR when component that uses useEffect (uses this custom hook) unmounts
+    return () => {
+      activeHttpRequests.current.forEach(abortCtrl => abortCtrl.abort());
+    };
+  }, []);
 
   return { isLoading, error, sendRequest, clearError };
 };
